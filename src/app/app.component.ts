cec9acf578276ae39a59ec7c06a847146cba27bf
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-08 15:54:41 +0200

    Code to Informing the User About a New Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Demo PWA with Angular';
+
+  constructor(
+    private swUpdate: SwUpdate
+  ) {}
+
+  ngOnInit() {
+    if ( this.swUpdate.isEnabled ) {
+      this.swUpdate.available.subscribe(
+        () => {
+          if ( confirm('New Version Available. Load new version?') ) {
+            window.location.reload();
+          }
+        }
+      );
+    }
+  }
+
 }
