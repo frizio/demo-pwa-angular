80615e93c02babc0686f1831128273ef53521b62
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-08 15:06:52 +0200

    Modification in manifest and a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Demo PWA Angular';
+  title = 'Demo PWA with Angular';
 }
